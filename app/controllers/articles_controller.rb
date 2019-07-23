48856b34c5954976f95e892a7e33bd999abd9dce
jordanhudgens <jordan@devcamp.com>
2019-07-22 19:19:49 -0700

    Populated the category fields to the controller whitelist, article form and added validations to the article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -75,6 +75,10 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content, :user_id)
+      params.require(:article).permit(
+        :title,
+        :content,
+        :category
+      )
     end
 end
